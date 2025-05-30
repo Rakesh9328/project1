66eaa2dc8453f9c7a9b90fd86c19de1d9d5671ca
Rakesh9328 <Rakesh9328@gmail.com>
2025-05-31 00:43:43 +0530

    i m devloping that code which not completed

---
+++ devlop/shell.sh
@@ -0,0 +1,16 @@
+#!/bin/bas 
+echo "enter file name"
+read fname
+
+if [ ! -e "$fname" ]; then
+	echo "create a file"
+	touch "fname"
+elif [ -e "$fname ]; then 
+	echo " $fname is exist in that directory "
+elif [ ! -s "$fname ]; then 
+	echo "there is no content please add content"
+	echo >> "fname"
+elif [ -s "fname" ]; then
+	echo " the file is exist along with content"
+else
+	echo "you have iput wrong file type"
